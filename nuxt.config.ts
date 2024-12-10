ae7742c056944cc91423fc6b9955b91a519059bf
boomieindahouse <prawitsanat@hotmail.com>
2024-12-11 00:54:48 +0700

    add font and add external style sheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   components: true,
-  css: ['@/assets/css/tailwind.css'],
+  css: [
+    '@/assets/css/tailwind.css',
+    '@/assets/css/styles.css'     
+  ],
   
   postcss: {
     plugins: {

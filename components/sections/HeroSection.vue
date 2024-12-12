b434e4b4c2e4de966b35097427c1385f63219d88
boomieindahouse <prawitsanat@hotmail.com>
2024-12-12 13:55:06 +0700

    edit herosection

--- components/sections/HeroSection.vue
+++ components/sections/HeroSection.vue
@@ -1,33 +1,43 @@
 <template>
-  <section class="bg-primarybg text-white py-10 sm:py-20">
-    <div class="container mx-auto flex flex-col-reverse sm:flex-row items-center">
+  <section class="bg-primarybg text-white py-10 sm:py-10">
+    <div
+      class="container mx-auto flex flex-col-reverse sm:flex-row items-center"
+    >
       <!-- ส่วนข้อความ -->
       <div class="w-full sm:w-1/2 text-center sm:text-left">
+        <h1 class="text-4xl sm:text-5xl font-bold mb-4">From Virtual Ideas</h1>
         <h1 class="text-4xl sm:text-5xl font-bold mb-4">
-          From Virtual Ideas
-        </h1>
-        <h1 class="text-4xl sm:text-5xl font-bold mb-4">
-          To Real 
+          To Real
           <span class="text-transparent bg-clip-text bg-gradient-text">
             Achievements
           </span>
         </h1>
         <p class="mb-8">
-          เราพร้อมที่จะดูแล และอยู่เคียงข้างคุณ <br>
+          เราพร้อมที่จะดูแล และอยู่เคียงข้างคุณ <br />
           เพื่อให้ธุรกิจของคุณเติบโตบนโลกออนไลน์
         </p>
-        <div class="flex flex-col sm:flex-row justify-center sm:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
-          <a href="/contact" class="bg-turqoise hover:bg-green-600 text-white font-medium py-3 px-6 rounded-md">
+        <div
+          class="flex flex-col sm:flex-row justify-center sm:justify-start space-y-4 sm:space-y-0 sm:space-x-4"
+        >
+          <a
+            href="/contact"
+            class="bg-turqoise hover:bg-green-600 text-white font-medium py-2 px-4 rounded-md">
             Line Contact
           </a>
-          <a href="/contact" class="bg-white hover:bg-gray-200 text-gray-900 font-medium py-3 px-6 rounded-md">
+          <a
+            href="/contact"
+            class="bg-white hover:bg-gray-200 text-gray-900 font-medium py-2 px-4 rounded-md">
             Telephone
           </a>
         </div>
       </div>
       <!-- ส่วนรูปภาพ -->
       <div class="w-full sm:w-1/2 mb-8 sm:mb-0">
-        <img src="../../assets/img/ex.png" alt="Hero Image" class="w-full max-w-sm sm:max-w-full mx-auto">
+        <img
+          src="../../assets/img/3.png"
+          alt="Hero Image"
+          class="w-full max-w-sm sm:max-w-full mx-auto"
+        />
       </div>
     </div>
   </section>

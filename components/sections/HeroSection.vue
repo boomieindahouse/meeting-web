e30efda8f00ef33fa31e262d677393fce19e874f
boomieindahouse <prawitsanat@hotmail.com>
2024-12-12 10:57:56 +0700

    add turqoise btn

--- components/sections/HeroSection.vue
+++ components/sections/HeroSection.vue
@@ -4,14 +4,14 @@
         <div class="w-1/2">
           <h1 class="text-5xl font-bold mb-4">From Virtual Ideas</h1>
           <h1 class="text-5xl font-bold mb-4">To Real <span class="text-transparent bg-clip-text bg-gradient-text">Achievements</span></h1>
-          <p class="mb-8">We are ready to support and stand by your side <br> to help your business thrive in the online world.</p>
+          <p class="mb-8">เราพร้อมที่จะดูแล และอยู่เคียงข้างคุณ <br> พื่อให้ธุรกิจของคุณเติบโตบนโลกออนไลน์</p>
           <div class="flex space-x-4">
-            <a href="/contact" class="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-6 rounded-md">Line Contact</a>
+            <a href="/contact" class="bg-turqoise hover:bg-green-600 text-white font-medium py-3 px-6 rounded-md">Line Contact</a>
             <a href="/contact" class="bg-white hover:bg-gray-200 text-gray-900 font-medium py-3 px-6 rounded-md">Telephone</a>
           </div>
         </div>
-        <div class="w-1/2">
-          <img src="#" alt="Hero Image" class="w-full">
+        <div class="w-max">
+          <img src="../../assets/img/com2-2.png" alt="Hero Image" class="w-full">
         </div>
       </div>
     </section>

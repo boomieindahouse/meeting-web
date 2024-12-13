6b8b5e05a2c8931f20f8f3a86fa475b170763d78
boomieindahouse <prawitsanat@hotmail.com>
2024-12-13 10:56:50 +0700

    edit z-index navbar to open menu in lg res

--- components/Layout/Navbar.vue
+++ components/Layout/Navbar.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav class="bg-navbg py-4 px-6">
+  <nav class="bg-navbg py-1 px-6">
     <div class="container mx-auto flex justify-between items-center">
       <!-- โลโก้ -->
       <nuxt-link to="/" class="text-gold font-bold text-xl">
-        Meeting Creative
+        <img
+          src="../../assets/img/logo2.png"
+          alt="Meeting Creative"
+          class="h-16"
+        />
       </nuxt-link>
 
       <!-- ปุ่ม Hamburger สำหรับ Mobile -->
@@ -21,31 +25,56 @@
       <ul
         :class="{
           'hidden lg:flex': !isMenuOpen,
-          'flex flex-col space-y-4 absolute top-16 left-0 w-full bg-navbg p-4': isMenuOpen
+          'flex flex-col space-y-4 absolute top-16 left-0 w-full bg-navbg p-4':
+            isMenuOpen,
         }"
         class="lg:space-x-10 lg:flex-row"
       >
-        <li class="py-2 text-center"><nuxt-link to="/" class="text-white hover:text-gold">Home</nuxt-link></li>
-        <li class="py-2 text-center"><nuxt-link to="/webdesign" class="text-white hover:text-gold">Web Design</nuxt-link></li>
-        <li class="py-2 text-center"><nuxt-link to="/graphic-design" class="text-white hover:text-gold">Graphic Design</nuxt-link></li>
-        <li class="py-2 text-center"><nuxt-link to="/network-service" class="text-white hover:text-gold">Network Service</nuxt-link></li>
-        <li class="py-2 text-center"><nuxt-link to="/our-team" class="text-white hover:text-gold">Our Team</nuxt-link></li>
-        <li class="py-2 text-center"><nuxt-link to="/contact" class="text-white hover:text-gold">Contact</nuxt-link></li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/" class="text-white hover:text-gold">Home</nuxt-link>
+        </li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/webdesign" class="text-white hover:text-gold"
+            >Web Design</nuxt-link
+          >
+        </li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/graphic-design" class="text-white hover:text-gold"
+            >Graphic Design</nuxt-link
+          >
+        </li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/network-service" class="text-white hover:text-gold"
+            >Network Service</nuxt-link
+          >
+        </li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/our-team" class="text-white hover:text-gold"
+            >Our Team</nuxt-link
+          >
+        </li>
+        <li class="py-2 text-center">
+          <nuxt-link to="/contact" class="text-white hover:text-gold"
+            >Contact</nuxt-link
+          >
+        </li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 const isMenuOpen = ref(false);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
-}
+};
 </script>
 
 <style scoped>
-/* เพิ่มการกำหนดสไตล์เพิ่มเติมหากจำเป็น */
+ul {
+  z-index: 100; /* เพิ่ม z-index เพื่อให้เมนูไม่ถูกปิดทับ */
+}
 </style>

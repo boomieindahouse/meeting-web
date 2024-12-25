6c94d5593c04786e04105f6b01bf12aa47c21ba1
boomieindahouse <prawitsanat@hotmail.com>
2024-12-25 15:17:00 +0700

    add our clients

--- pages/webdesign.vue
+++ pages/webdesign.vue
@@ -1,6 +1,7 @@
 <script setup>
 import WebRecentworks from '@/components/sections/web-design/WebRecentworks.vue'
 import WebCategory from '@/components/sections/web-design/WebCategory.vue'
+import ClientLogoSlider from '~/components/common/ClientLogoSlider.vue'
 
 </script>
 
@@ -8,6 +9,7 @@ import WebCategory from '@/components/sections/web-design/WebCategory.vue'
     <div>
         <WebRecentworks />
         <WebCategory />
+        <ClientLogoSlider />
     </div>
 </template>
 
